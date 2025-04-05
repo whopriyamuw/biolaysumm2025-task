ca6d8d9f904403ed521427f01b21bce46245c203
whopriyamuw <pbasu77@uw.edu>
2025-04-05 03:16:34 -0700

    summarisation code added

--- download_datasets.py
+++ download_datasets.py
@@ -3,12 +3,17 @@
 from datasets import load_dataset
 
 # Load the dataset (make sure you're logged in if it's gated)
-ds = load_dataset("BioLaySumm/BioLaySumm2025-PLOS")
+ds1 = load_dataset("BioLaySumm/BioLaySumm2025-PLOS")
+ds2 = load_dataset("BioLaySumm/BioLaySumm2025-eLife")
 
 # Specify the local directory for export
-output_dir = "biolaysumm_dataset"
+output_dir1 = "biolaysumm_dataset/plos"
+output_dir2 = "biolaysumm_dataset/elife"
 
 # Export each split to local files (e.g., train, test)
-for split in ds:
-    # You can also use .to_json or .to_parquet here
-    ds[split].to_csv(f"{output_dir}/{split}.csv", index=False)
+for split in ds1:
+    ds1[split].to_csv(f"{output_dir1}/{split}.csv", index=False)
+
+for split in ds2:
+    ds2[split].to_csv(f"{output_dir2}/{split}.csv", index=False)
+
