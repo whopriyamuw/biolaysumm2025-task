1b0b7b36c7d8f3ec3d18db49ef6abefcba4654e6
Yongsin Park <yongsin.nlp@gmail.com>
2025-04-21 01:16:08 -0700

    Removed unused files

--- condor/finetune.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-~/miniconda3/envs/llama3/bin/python ~/projects/biolaysumm2025/finetune/finetune.py "$@"
--- preprocess/biolaysumm_dataset.py
+++
@@ -1,3 +0,0 @@
-from torchtune.datasets import samsum_dataset as biolaysumm_dataset
-
-dataset = biolaysumm_dataset()
